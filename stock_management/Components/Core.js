01c5250b83e0db6909ff2699f9b846c9252abfd8
Shaunak <ssnagvenkar@gmail.com>
2023-11-14 18:10:48 +0530

    Initial

---
+++ stock_management/Components/Core.js
@@ -0,0 +1,127 @@
+"use client"
+
+import React, {useState} from 'react';
+
+const ingredients = [
+  {
+    name: 'Tshirt',
+    quantity: 5,
+    price: 200
+  }
+]
+
+const Core = () => {
+
+  // const [prod, setProd] = useState({})
+
+  // const handleChange = (e) => {
+  //   setProd({...prod, [e.target.name]: e.target.value})
+  // }
+
+  // const addP = async (e) => {
+  //   e.preventDefault();
+  
+  //   try {
+  //     const response = await fetch('http://localhost:3000/api/products', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify(prod),
+  //     });
+  
+  //     if (response.ok) {
+  //       console.log('Product added successfully');
+  //     } else {
+  //       const errorText = await response.text();
+  //       console.error('Server error:', errorText);
+  //       console.log('Not Successfully');
+  //     }
+  //   } catch (error) {
+  //     console.error('Client error:', error);
+  //   }
+  // };  
+    
+  // return (
+  //   <>
+  //   <div className="m-10">
+  //     <div>
+  //       <h1 className="section-name bg-pink-100 mx-auto px-2 rounded m-5">Current Stock</h1>
+  //     </div>
+  //     <div>
+  //       <div className="flex items-center mb-4">
+  //         <input
+  //           type="text"
+  //           placeholder="Search for a product..."
+  //           className="border p-2 rounded-l"
+  //           // onChange={(e) => setSearchTerm(e.target.value)}
+  //         />
+  //         <button className="bg-blue-500 text-white p-2 rounded-r">
+  //           Search
+  //         </button>
+  //       </div>
+  //       <input
+  //           id='name'
+  //           name='name'
+  //           type="text"
+  //           placeholder="Enter the name"
+  //           className="border p-2 rounded-l"
+  //           onChange={handleChange}
+  //         />
+  //         <input
+  //           id='quantity'
+  //           name='quantity'
+  //           type="text"
+  //           placeholder="Enter the qty"
+  //           className="border p-2 rounded-l"
+  //           onChange={handleChange}
+  //         />
+  //         <input
+  //           id='price'
+  //           name='price'
+  //           type="text"
+  //           placeholder="Enter the price"
+  //           className="border p-2 rounded-l"
+  //           onChange={handleChange}
+  //         />
+  //       <button onClick={addP} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">
+  //         Add new item
+  //       </button>
+  //       <input
+  //           type="text"
+  //           placeholder="Search for a product..."
+  //           className="border p-2 rounded-l"
+  //           // onChange={(e) => setSearchTerm(e.target.value)}
+  //         />
+  //       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  //         Delete item
+  //       </button>
+  //     </div>
+  //     <table className="min-w-full bg-white border border-gray-300 rounded">
+  //       <thead>
+  //         <tr>
+  //           <th className="py-2 px-4 border-b">Ingredient</th>
+  //           <th className="py-2 px-4 border-b">Stock Quantity</th>
+  //           <th className="py-2 px-4 border-b">Price</th>
+  //         </tr>
+  //       </thead>
+  //       <thead>
+  //         {ingredients.map((ingredient, index) => (
+  //           <tr key={index}>
+  //             <td className="py-2 px-4 border-b text-center">{ingredient.name}</td>
+  //             <td className="py-2 px-4 border-b text-center">
+  //               <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-2 rounded mx-1"> - </button>
+  //               {ingredient.quantity}
+  //               <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-2 rounded mx-1"> + </button>
+  //             </td>
+  //             <td className="py-2 px-4 border-b text-center">{ingredient.price}</td>
+  //           </tr>
+  //         ))}
+  //       </thead>
+  //     </table>
+  //   </div>
+  //   </>
+  // );
+};
+
+export default Core;
---
+++ stock_management/app/api/products/route.js
@@ -0,0 +1,31 @@
+import { MongoClient } from 'mongodb';
+import { NextResponse } from 'next/server';
+
+export async function GET(request) {
+  const url = 'mongodb+srv://shaunak:6eEvklyHKzskKLda@InventoryManagementCluster.mongodb.net/';
+  const client = new MongoClient(url);
+  try {  
+    const database = client.db('INVENTORY');
+    const inv = database.collection('inventory');
+    const query = { };
+    const allProd = await inv.find(query).toArray();  
+    return NextResponse.json({ allProd });
+  } finally {
+    await client.close();
+  }
+}
+
+export async function POST(request) {
+  try {
+    let body = await request.json();
+    const url = 'mongodb+srv://shaunak:6eEvklyHKzskKLda@InventoryManagementCluster.mongodb.net/';
+    const client = new MongoClient(url);
+    const database = client.db('INVENTORY');
+    const inv = database.collection('inventory');
+    const query = { };
+    const allProd = await inv.insertOne(body);
+    return NextResponse.json({ allProd, ok: true });
+  } finally {
+    await client.close();
+  }
+}
